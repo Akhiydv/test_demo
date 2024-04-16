5aba8f9bb9791e11593db23921f86b26d88a12dc
Akhiydv <yadavakhilesh@gmail.com>
2024-04-17 00:04:28 +0530

    new commit

---
+++ ReverseArray.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ReverseArray
+{
+ public static void main(String args[])
+ {
+  int arr[]={1,2,3,4,5,6}; 
+  //int arr[]={8,1,5,4,15,10};
+  int n=arr.length;
+    
+  for(int i=n-1; i>=0; i--)
+    {
+      System.out.print(arr[i]+" "); 
+    }
+ }
+}
