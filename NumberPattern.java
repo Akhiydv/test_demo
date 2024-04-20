3e1e3102351e5f8327cc9fdd5321e407baccea79
Akhiydv <yadavakhilesh@gmail.com>
2024-04-20 07:50:10 +0530

    new commit

---
+++ NumberPattern.java
@@ -0,0 +1,24 @@
+public class NumberPattern 
+{
+    public static void main(String[] args)
+	{
+        int rows=5; // Number of rows in the pattern
+
+        // Outer loop for rows
+        for(int i=1; i<=rows; i++)
+		{
+         // Inner loop for spaces
+         for(int j=1; j<=rows-i; j++) 
+			{
+                System.out.print(" ");
+            }
+         // Inner loop for numbers
+         for(int k=1; k<=i*2-1; k++)
+			{
+                System.out.print(i+" ");
+            }
+	    // Move to the next line after each row
+        System.out.println();
+        }
+    }
+}
