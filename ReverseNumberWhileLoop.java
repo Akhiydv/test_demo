0f0b13c2c77ee1e53e28edaf3240dec41ab03bb2
Akhiydv <yadavakhilesh@gmail.com>
2024-05-02 10:27:09 +0530

    new commit

---
+++ ReverseNumberWhileLoop.java
@@ -0,0 +1,15 @@
+class ReverseNumberWhileLoop
+{
+ public static void main(String args[])
+ {
+ int num=1234, rev=0, rem;
+ 
+ while(num!=0)
+ {
+  rem=num%10;
+  rev=rev*10+rem;
+  num/=10;
+ }
+ System.out.println("Reverse number is : "+rev);
+ }
+}
