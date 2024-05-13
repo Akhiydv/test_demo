5e6f9bb5073eb2ccba23fcfb6e28cf40611c3daa
Akhiydv <yadavakhilesh@gmail.com>
2024-05-13 17:14:53 +0530

    new commit

---
+++ SumOfOneDimensionalArray.java
@@ -0,0 +1,13 @@
+class SumOfOneDimensionalArray
+{
+ public static void main(String args[])
+ {
+  int arr[]={1,2,3,4};
+  int sum=0;
+  for(int i=0;i<arr.length;i++)
+  {
+   sum=sum+arr[i];
+  }
+  System.out.println("Sum of the Elements off an Array :"+sum);
+ }
+}
