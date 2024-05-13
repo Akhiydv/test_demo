a066c95c843930f3fcdd4e888e66c170721cf9af
Akhiydv <yadavakhilesh@gmail.com>
2024-05-13 11:38:11 +0530

    new commit

---
+++ StringSimple.java
@@ -0,0 +1,10 @@
+class StringSimple
+{
+ public static void main(String args[])
+ {
+  String str="Akhilesh";
+  System.out.println("1st method of String literal :"+str);
+  String str1=new String("Yadav");
+  System.out.println("2nd method of new keyword :"+str1); 
+ }
+}
