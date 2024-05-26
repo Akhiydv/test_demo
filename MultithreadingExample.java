ca53d97df8b4bffc6f87ea23c17a1ccfe4769171
Akhiydv <yadavakhilesh@gmail.com>
2024-05-26 14:11:37 +0530

    new commit

---
+++ MultithreadingExample.java
@@ -0,0 +1,60 @@
+// Define a class that implements the Runnable interface
+class NumberPrinter implements Runnable 
+{
+    private String threadName;
+
+    // Constructor to set the thread name
+    public NumberPrinter(String threadName)
+	{
+        this.threadName = threadName;
+    }
+
+    @Override
+    public void run()
+	{
+        try 
+		{
+            // Print numbers from 1 to 5 with a delay
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(threadName + " - Number: " + i);
+                // Sleep for 500 milliseconds
+                Thread.sleep(500);
+        }
+        } 
+		catch(InterruptedException e)
+		{
+            System.out.println(threadName + " interrupted.");
+        }
+        System.out.println(threadName + " finished.");
+    }
+}
+
+public class MultithreadingExample
+{
+ public static void main(String args[])
+ {
+        // Create instances of NumberPrinter for two threads
+        Runnable numberPrinter1 = new NumberPrinter("Thread 1");
+        Runnable numberPrinter2 = new NumberPrinter("Thread 2");
+
+        // Create Thread objects
+        Thread thread1 = new Thread(numberPrinter1);
+        Thread thread2 = new Thread(numberPrinter2);
+
+        // Start the threads
+        thread1.start();
+        thread2.start();
+
+        // Wait for both threads to finish
+        try 
+		{
+            thread1.join();
+            thread2.join();
+        } 
+		catch(InterruptedException e)
+		{
+            System.out.println("Main thread interrupted.");
+        }
+     System.out.println("Both threads have finished.");
+    }
+}
