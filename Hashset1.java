36c30e35720f43f82fc98b2428fb7140ba32b80f
Akhiydv <yadavakhilesh@gmail.com>
2024-07-01 09:04:46 +0530

    new commit

---
+++ Hashset1.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+class Hashset1{
+       public static void main(String[] args){
+	    HashSet<String> ss=new HashSet<>();
+		  ss.add("dd");
+		  ss.add("dd");
+		  ss.add("bddnw");
+		  ss.add("ddwnbem");
+		  System.out.println(ss);
+		
+	   }
+}
