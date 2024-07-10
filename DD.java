475be885079274a42d8e46380009e28b0e81ffc9
Akhiydv <yadavakhilesh@gmail.com>
2024-07-10 18:49:33 +0530

    new commit 10July6:50PM

---
+++ DD.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import java.util.HashMap;
+class DD
+{
+    public static void main(String[] args)
+	{
+	   HashMap<String,String> ss = new HashMap<String,String>();
+	   ss.put("1","vhifow");
+	   ss.put("2","hello");
+	   ss.put("3","www");
+	   System.out.println(ss);
+	}
+}
