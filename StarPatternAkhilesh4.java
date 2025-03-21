deb9308704c721c5b13e033b490977e93b83eda5
Akhiydv <yadavakhilesh@gmail.com>
2025-03-21 22:11:38 +0530

    NewCommit21March202510:11PM

---
+++ StarPatternAkhilesh4.java
@@ -0,0 +1,22 @@
+public class StarPatternAkhilesh4
+{
+ public static void main(String args[])
+ {
+    String[] akhileshPattern = new String[7];
+
+    // 'A' pattern
+    akhileshPattern[0] = "  *   *   *    *   *   *  *       ****** ******  *    *";
+    akhileshPattern[1] = " * *  *  *     *   *   *  *       *      *       *    *";
+    akhileshPattern[2] = "*   * * *      *   *   *  *       *      *       *    *";
+    akhileshPattern[3] = "***** **       *****   *  *       ****** ******  ******";
+    akhileshPattern[4] = "*   * * *      *   *   *  *       *           *  *    *";
+    akhileshPattern[5] = "*   * *  *     *   *   *  *       *           *  *    *";
+    akhileshPattern[6] = "*   * *   *    *   *   *  ******  ****** ******  *    *";
+
+    // Printing the pattern for "Akhilesh"
+    for (int i = 0; i < akhileshPattern.length; i++)
+	{
+      System.out.println(akhileshPattern[i]);
+    }
+ }
+}
